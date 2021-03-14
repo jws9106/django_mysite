4c216793c914ed7a091627b5ec00e1fb2d390363
jws9106 <jws2920@naver.com>
2021-03-14 19:47:34 +0900

    make question_detail

--- pybo/views.py
+++ pybo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -15,3 +15,12 @@ def index(request):
     context = {'question_list': question_list}
 
     return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    #question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question':question}
+    return render(request, 'pybo/question_detail.html', context)
